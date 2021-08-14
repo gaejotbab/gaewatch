e67582b3a030265fa6bc0d9fab4f88fc02f488e0
개좆밥 <gaejotbab@gmail.com>
2021-08-14 22:14:03 +0900

    App과 Scaffold 도입하고 TopAppBar 꾸며줌

--- app/src/main/java/io/gaejotbab/gaewatch/MainActivity.kt
+++ app/src/main/java/io/gaejotbab/gaewatch/MainActivity.kt
@@ -3,9 +3,7 @@ package io.gaejotbab.gaewatch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import io.gaejotbab.gaewatch.ui.theme.GaewatchTheme
@@ -14,25 +12,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GaewatchTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            App()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun App() {
+    GaewatchTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(title = { Text(text = "개워치") })
+            },
+            content = { paddingValues ->
+            }
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    GaewatchTheme {
-        Greeting("Android")
-    }
+fun AppPreview() {
+    App()
 }
